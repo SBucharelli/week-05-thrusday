1964111d4ccc963ce7ffee7e76bf30d5b7d06f23
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-02 20:00:22 -0400

    Write code for xor() and all() functions

--- all.js
+++ all.js
@@ -1,6 +1,19 @@
 function all(arr) {
 	// Loop through every item in the array
 	// If the item evaluates to false, return false
-
-	// If the loop ends, return true
+	for (let i = 0; i < arr.length; i++) { // i = i + 1
+		if (arr[i] == false) {
+			return false
+		}
+	}
+	return true
 }
+
+// If the loop ends, return true
+
+
+let array1 = [true, false, false]
+let array2 = [true, true, true] //length is 3
+
+console.log(all(array1))
+console.log(all(array2))
